a040605f1f3516f4c6828a669ee8c03ad39ce0da
JyotiDaiya <49122733+JyotiDaiya@users.noreply.github.com>
2019-04-02 22:51:18 +0530

    file upload and get manufacturer by API

--- src/app/car-model/car-model.component.ts
+++ src/app/car-model/car-model.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Manufacturer } from '../shared';
+import { ManufacturerService } from '../shared';
 
 @Component({
   selector: 'app-car-model',
@@ -11,10 +12,32 @@ export class CarModelComponent implements OnInit {
 	
 	manufacturers: Manufacturer[] = [];
 	
-	constructor() { }
+	imgpreview;
+	
+	constructor(private m: ManufacturerService) { 
+	
+		m.getManufacturer().subscribe((res) => {
+			
+			this.manufacturers = res;
+			
+		});
+	
+	}
 	
 	ngOnInit() {
 		
+	}
+	
+	public fileChangeEvent(fileInput: any){
+		if (fileInput.target.files && fileInput.target.files[0]) {
+			const reader = new FileReader();
+			
+			reader.onload = ((e) => {
+				this.imgpreview = e.target['result'];
+			});
+			
+			reader.readAsDataURL(fileInput.target.files[0]);
+		}
 	}
 
 }
